294784d8f8285409a0db93f0348f62a58ab23d34
Roland Heusser <skofgar@gmail.com>
2015-12-05 01:18:23 -0500

    make struct hashable

--- Fandango/Model/Movie.swift
+++ Fandango/Model/Movie.swift
@@ -7,14 +7,22 @@
 
 import Foundation
 
-struct Movie {
+struct Movie : Hashable {
     let name: String
     let showtime: String
     let theatre: String
+    
+    var hashValue: Int {
+        return name.hashValue ^ showtime.hashValue ^ theatre.hashValue
+    }
     
     init(name: String, theatre: String, showtime: String) {
         self.name = name
         self.theatre = theatre
         self.showtime = showtime
     }
+}
+
+func ==(left: Movie, right: Movie) -> Bool {
+    return left.name == right.name && left.showtime == right.showtime && left.theatre == right.theatre
 }
