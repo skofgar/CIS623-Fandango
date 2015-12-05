f9cb827271bb3d76db50b77f32899ae8c5125446
Roland Heusser <skofgar@gmail.com>
2015-12-05 00:40:45 -0500

    LanguageParser passing dictionaries by reference now

--- Fandango/Model/LanguageParser.swift
+++ Fandango/Model/LanguageParser.swift
@@ -25,29 +25,27 @@ class LanguageParser {
         var results = MovieDictionary()
         
         for token in tokens {
-            results = checkAndAddToDictonary(MovieCollection.sharedInstance.lookupByMovie(token), dic: results)
-            results = checkAndAddToDictonary(MovieCollection.sharedInstance.lookupByTheatre(token), dic: results)
-            results = checkAndAddToDictonary(MovieCollection.sharedInstance.lookupByShowTime(token), dic: results)
+            checkAndAddToDictonary(MovieCollection.sharedInstance.lookupByMovie(token), dic: &results)
+            checkAndAddToDictonary(MovieCollection.sharedInstance.lookupByTheatre(token), dic: &results)
+            checkAndAddToDictonary(MovieCollection.sharedInstance.lookupByShowTime(token), dic: &results)
         }
         return results
     }
     
-    func checkAndAddToDictonary(movies: [Movie]?, dic: MovieDictionary) -> MovieDictionary {
+    func checkAndAddToDictonary(movies: [Movie]?, inout dic: MovieDictionary) {
         print(movies)
         if (nil != movies) {
-            return addToDictionary(movies!, dic: dic)
+            addToDictionary(movies!, dic: &dic)
         }
-        return dic
     }
     
-    func addToDictionary(movies: [Movie], var dic: MovieDictionary) -> MovieDictionary {
+    func addToDictionary(movies: [Movie], inout dic: MovieDictionary)  {
         for movie in movies {
             if (nil == dic[movie.name]) {
                 // TODO: currently overwriting the movie (instead of storing all alternative possibilites)
                 dic[movie.name] = movie
             }
         }
-        return dic
     }
 }
 
